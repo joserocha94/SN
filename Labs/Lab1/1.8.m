14d8eff91d0ae4803b2a67a9466e46485d2f4e0c
José Rocha <48589830+joserocha94@users.noreply.github.com>
2022-03-23 23:47:15 +0000

    Update 1.8.m

--- Labs/Lab1/1.8.m
+++ Labs/Lab1/1.8.m
@@ -1,6 +1,7 @@
 % ========================================
 % Exercise 7 – Cumulative Elevation Gain
 % ========================================
+
 % Compute the cumulative elevation (altitude) gain on the track.
 % Use only VTG sentences with the Mode Indicator field equal to ‘A’ or ‘D’, and a valid checksum 
 % (in this case all the sentences in the file have a valid checksum).
