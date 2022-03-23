c7db7553b1e5d5be64a7c000a241f98864847cfb
José Rocha <48589830+joserocha94@users.noreply.github.com>
2022-03-23 23:32:44 +0000

    Update dms2dm.m

--- Toolbox/dms2dm.m
+++ Toolbox/dms2dm.m
@@ -1,5 +1,5 @@
-#receives 3 args: degrees, minutes and seconds and
-# returns the equivalent value em degrees and minutes
+#receives 3 args: degrees, minutes and seconds
+#returns the equivalent value em degrees and minutes
 
 function [degrees, minutes] = dms2dm(d, m, s)
   
