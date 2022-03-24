97dd82ab650c2ea592615ab0a327550493c07868
José Rocha <48589830+joserocha94@users.noreply.github.com>
2022-03-24 00:25:31 +0000

    Update dms2dm.m

--- Toolbox/dms2dm.m
+++ Toolbox/dms2dm.m
@@ -2,14 +2,14 @@
 % returns the equivalent value in degrees and minutes
 
 function [degrees, minutes] = dms2dm(d, m, s)
-  
-  minutes = s/60;   
-  if m < 0
-      minutes = m-minutes;
-  else
-      minutes = minutes+m;
-  end
-  degrees = d;
-  
+
+    minutes = s/60;   
+    if m < 0
+        minutes = m-minutes;
+    else
+        minutes = minutes+m;
+    end
+    degrees = d;
+    
 endfunction
 
