d89b185ed8fc4f954d32e6f8c865bdb78fe6707c
José Rocha <48589830+joserocha94@users.noreply.github.com>
2022-04-05 22:37:27 +0100

    Update 3.1.m

--- Labs/Lab3/3.1.m
+++ Labs/Lab3/3.1.m
@@ -3,7 +3,7 @@
 %             Exercise 3
 % ===================================================
 
-%Consider a satellite with the following orbital parameters
+% Consider a satellite with the following orbital parameters
 %   > orbit semi-major axis: A = 26559755m;
 %   > orbit eccentricity: e = 0.017545;
 %   > argument of perigee: ? = 1.626021rad.Consider a satellite with the following orbital parameters
@@ -16,40 +16,40 @@
   e = 0.017545;
   w = 1.626021;
 
-  % alínea a)
+  % alÃ­nea a)
   t = 39929;
   miu = 3.986005*10^14;
   T = 2*pi*sqrt((A^3)/miu);
   fprintf('Orbital period:\n%f s\n%f m\n%f h\n\n', T, T/60, T/3600);
 
-  % alínea b)
-  eta=2*pi/T;
-  fprintf('Mean angular motion:\n%f rad/s\n%f º/s\n%f º/h\n\n', eta, eta*180/pi, eta*180/pi*3600);
+  % alÃ­nea b)
+  eta = 2*pi/T;
+  fprintf('Mean angular motion:\n%f rad/s\n%f Âº/s\n%f Âº/h\n\n', eta, eta*180/pi, eta*180/pi*3600);
 
-  % alínea c)
-  M=eta*t;
-  fprintf('Mean anomaly:\n%f rad/s\n%f º\n\n',M,M*180/pi);
+  % alÃ­nea c)
+  M = eta*t;
+  fprintf('Mean anomaly:\n%f rad/s\n%f Âº\n\n', M, M*180/pi);
 
-  % alínea d)
+  % alÃ­nea d)
   t = 10^-12;
   E = M;
   while (1)
-      Ep=E;
-      E=M+e*sin(E);
-      if abs(E-Ep)<t
+      Ep = E;
+      E = M + e * sin(E);
+      if abs(E - Ep) < t
           break
       else
           continue
       end
   end
-  fprintf('Eccentric anomaly:\n%f rad/s\n%f º\n\n', E, E*180/pi);
+  fprintf('Eccentric anomaly:\n%f rad/s\n%f Âº\n\n', E, E*180/pi);
   
-  % alínea e)
+  % alÃ­nea e)
   phi0 = atan2(sqrt(1-e^2)*sin(E),cos(E)-e);
   r = A*(1-e*cos(E));
-  fprintf('True anomaly:\n%f rad\n%f º\n', phi0, phi0*180/pi);
+  fprintf('True anomaly:\n%f rad\n%f Âº\n', phi0, phi0*180/pi);
   fprintf('Radius:\n%f m\n\n', r);
   
-  % alínea f)
-  phi=phi0+w;
-  fprintf('Argument of latitude:\n%f rad\n%f º\n\n', phi, phi*180/pi);
+  % alÃ­nea f)
+  phi = phi0 + w;
+  fprintf('Argument of latitude:\n%f rad\n%f Âº\n\n', phi, phi*180/pi);
