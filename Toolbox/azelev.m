0104e705f305425b913ef9bcf73add6034b908f3
José Rocha <48589830+joserocha94@users.noreply.github.com>
2022-04-08 14:58:35 +0100

    Update azelev.m

--- Toolbox/azelev.m
+++ Toolbox/azelev.m
@@ -1,9 +1,10 @@
 
 % AZELEV: Calculates azimuth and elevation
-function [az,el] = azelev(E,N,U)
+function [az, el] = azelev(E, N, U)
 
 %   Outputs azimuth and elevation (in degrees) given ENU coordinates in
 %   meters.
+%   ATTETION: not xyz coordinates (!!!)
 
   az = atan2(E, N)*180/pi;
   el = atan2(U, sqrt(N^2+E^2))*180/pi;
