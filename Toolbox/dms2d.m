b482a6f52053831559653651e51996a02f19fa62
José Rocha <48589830+joserocha94@users.noreply.github.com>
2022-04-11 14:35:55 +0100

    Update dms2d.m

--- Toolbox/dms2d.m
+++ Toolbox/dms2d.m
@@ -1,14 +1,17 @@
-function [degrees] = dms2d(d,m,s)
-%DMS2D Convert degrees, minutes and seconds to degrees with decimal places
-% Having as arguments the degrees, minutes and (optional, defaults to zero)
-% seconds of an angular measurement, outputs the value in degrees with
-% decimal places.
 
-if nargin==2    % Set the default value for seconds
-    s=0;
+% DMS2D 
+function [degrees] = dms2d(d,m,s)
+
+%   Convert degrees, minutes and seconds to degrees with decimal places
+%   Having as arguments the degrees, minutes and (optional, defaults to zero)
+%   seconds of an angular measurement, outputs the value in degrees with
+%   decimal places.
+
+if nargin==2    
+    s=0; % default value
 end
 
-minutes=s/60;       % Usual conversions
+minutes=s/60;      
 
 if m<0
     minutes=m-minutes;
@@ -17,7 +20,7 @@ else
 end
 degrees=minutes/60;
 
-if d<0  % Signal of the output based on input.
+if d<0  
     degrees=d-degrees;
 elseif d>=0
     degrees=d+degrees;
