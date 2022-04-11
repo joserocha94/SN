84b9c355e3098c660f04ed73b00a339b8376cda3
José Rocha <48589830+joserocha94@users.noreply.github.com>
2022-04-11 14:36:25 +0100

    Update ecef2enu.m

--- Toolbox/ecef2enu.m
+++ Toolbox/ecef2enu.m
@@ -1,13 +1,14 @@
+% ECEF2ENU ECEF to ENU conversion
 function [E,N,U] = ecef2enu(X1,Y1,Z1,lat1,lon1,X2,Y2,Z2)
-%ECEF2ENU ECEF to ENU conversion
+
 %   Converts ECEF coordinates (X2,Y2,Z2) (in meters) to an ENU frame
 %   relative to point 1.
 
-dx=X2-X1;
-dy=Y2-Y1;
-dz=Z2-Z1;
+  dx=X2-X1;
+  dy=Y2-Y1;
+  dz=Z2-Z1;
 
-[E,N,U]=rotz(dx,dy,dz,lon1+90);
-[E,N,U]=rotx(E,N,U,90-lat1);
+  [E,N,U]=rotz(dx,dy,dz,lon1+90);
+  [E,N,U]=rotx(E,N,U,90-lat1);
 
 end
