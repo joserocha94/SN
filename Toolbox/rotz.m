a56c18e17878140c0ff17aa56db650c77d7f9ed4
José Rocha <48589830+joserocha94@users.noreply.github.com>
2022-04-11 14:38:28 +0100

    Update rotz.m

--- Toolbox/rotz.m
+++ Toolbox/rotz.m
@@ -1,11 +1,11 @@
+% ROTZ Rotation about z axis
 function [X2,Y2,Z2] = rotz(X1,Y1,Z1,gamma)
-%ROTZ Rotation about z axis
 %   Rotates vector [X1,Y1,Z1] about by gamma degrees, about the z axis.
 
-gamma=gamma*pi/180;
+  gamma=gamma*pi/180;
 
-X2=X1*cos(gamma)+Y1*sin(gamma);
-Y2=-X1*sin(gamma)+Y1*cos(gamma);
-Z2=Z1;
+  X2=X1*cos(gamma)+Y1*sin(gamma);
+  Y2=-X1*sin(gamma)+Y1*cos(gamma);
+  Z2=Z1;
 
 end
