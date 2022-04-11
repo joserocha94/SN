bcb46b1415423478eb8ca49b2410fc2fb5004105
José Rocha <48589830+joserocha94@users.noreply.github.com>
2022-04-11 14:37:01 +0100

    Update llh2xyz.m

--- Toolbox/llh2xyz.m
+++ Toolbox/llh2xyz.m
@@ -1,8 +1,4 @@
 
-% p1 = (38º 46´ 49.61´´ N, 9º 29´ 56.19´´ W, 103 m)
-% The above format of the latitude and longitude will be referred to as DDº MM´ SS.ss´´.
-% Rewrite those coordinates in the following formats: a) DDº MM.mmm´; b) DD.dddº
-
 tb_dms2dm.m
 
 p1lat = [38 46 49.61];
@@ -14,3 +10,5 @@ h1 = 103;
 
 fprintf('Latitude: %d deg %f min\n', dlat, mlat);
 fprintf('Longitude: %d deg %f min\n', dlon, mlon);
+
+%to do: fix this  code
