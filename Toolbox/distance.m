5870e3b795661b5f4f18032f44fe6ccf36f85436
José Rocha <48589830+joserocha94@users.noreply.github.com>
2022-04-11 14:34:50 +0100

    Update distance.m

--- Toolbox/distance.m
+++ Toolbox/distance.m
@@ -1,6 +1,6 @@
-%DISTANCE Compute the distance between two points
+% DISTANCE Compute the distance between two points
 function [d] = distance(X1,Y1,Z1,X2,Y2,Z2)
-%   Detailed explanation goes here
 
     d=sqrt((X2-X1)^2+(Y2-Y1)^2+(Z2-Z1)^2);
+    
 end
