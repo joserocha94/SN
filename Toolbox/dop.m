9f85323b02a01f87ac8bbea5bc9e9259d8d8e048
José Rocha <48589830+joserocha94@users.noreply.github.com>
2022-04-27 22:49:17 +0100

    Add files via upload

---
+++ Toolbox/dop.m
@@ -0,0 +1,17 @@
+function DOP = dop(H,i)
+%DOP Calculate the dilution of precision
+%   Calculates the dilution of precision associated with a matrix H.
+
+F=inv(H'*H);
+
+S=F(1:i,1:i);
+
+DOP=0;
+
+for d=1:i;
+    DOP=DOP+S(d,d);
+end
+
+DOP=sqrt(DOP);
+    
+end
